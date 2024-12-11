be0de176a100fee3d63604b4763c9887f1cabf01
Thurston Yates <thurstonalexyates@gmail.com>
2024-12-11 01:03:23 -0500

    Recursive

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -9,31 +9,54 @@ fn load_input(path: &str) -> Vec<i64> {
     stones
 }
 
-fn puzzle(stones: &Vec<i64>, blink_times: i64) -> i64 {
-    let mut new_stones = stones.clone();
+fn process_stone(blink_times: i32, blinks: i32, stone: i64) -> i64 {
+    if blinks == blink_times {
+        return 1;
+    }
+    let as_str = stone.to_string();
 
-    for i in 0..blink_times {
-        let mut s = 0;
-        while s < new_stones.len() {
-            let val = new_stones[s];
-            let as_str = val.to_string();
-            if val == 0 {
-                new_stones[s] = 1;
-                s += 1;
-            } else if as_str.len() % 2 == 0 {
-                let first = &as_str[..(as_str.len() / 2)];
-                let second = &as_str[(as_str.len() / 2)..as_str.len()];
-                new_stones[s] = first.parse::<i64>().expect("First not a number!");
-                new_stones.insert(s + 1, second.parse::<i64>().expect("Second not a number!"));
-                s += 2;
-            } else {
-                new_stones[s] *= 2024 as i64;
-                s += 1;
-            }
-        }
+    if stone == 0 {
+        return process_stone(blink_times, blinks + 1, 1);
+    } else if as_str.len() % 2 == 0 {
+        let first_s = &as_str[..(as_str.len() / 2)];
+        let second_s = &as_str[(as_str.len() / 2)..as_str.len()];
+        let first = first_s.parse::<i64>().expect("First not a number!");
+        let second = second_s.parse::<i64>().expect("Second not a number!");
+        return process_stone(blink_times, blinks + 1, first)
+            + process_stone(blink_times, blinks + 1, second);
+    } else {
+        return process_stone(blink_times, blinks + 1, stone * 2024);
+    }
+}
+
+fn puzzle(stones: &Vec<i64>, blink_times: i32) -> i64 {
+    let mut sum = 0;
+    for stone in stones {
+        sum += process_stone(blink_times, 0, *stone);
     }
 
-    new_stones.len() as i64
+    sum
+
+    // for i in 0..blink_times {
+    //     let mut s = 0;
+    //     while s < new_stones.len() {
+    //         let val = new_stones[s];
+    //         let as_str = val.to_string();
+    //         if val == 0 {
+    //             new_stones[s] = 1;
+    //             s += 1;
+    //         } else if as_str.len() % 2 == 0 {
+    //             let first = &as_str[..(as_str.len() / 2)];
+    //             let second = &as_str[(as_str.len() / 2)..as_str.len()];
+    //             new_stones[s] = first.parse::<i64>().expect("First not a number!");
+    //             new_stones.insert(s + 1, second.parse::<i64>().expect("Second not a number!"));
+    //             s += 2;
+    //         } else {
+    //             new_stones[s] *= 2024 as i64;
+    //             s += 1;
+    //         }
+    //     }
+    // }
 }
 
 fn main() {
