571ae2cf2d37cbd1a78fae567b7d49e3fcc1d57b
Thurston Yates <thurstonalexyates@gmail.com>
2024-12-11 01:54:38 -0500

    Make memoized

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 fn load_input(path: &str) -> Vec<i64> {
     let contents = fs::read_to_string(path).expect("Failed to read file");
@@ -9,54 +9,53 @@ fn load_input(path: &str) -> Vec<i64> {
     stones
 }
 
-fn process_stone(blink_times: i32, blinks: i32, stone: i64) -> i64 {
-    if blinks == blink_times {
+fn process_stone(
+    blink_times: i32,
+    blinks: i32,
+    stone: i64,
+    cache: &mut HashMap<(i64, i32), i64>,
+) -> i64 {
+    let map_val = cache.get(&(stone, blinks));
+
+    if blinks >= blink_times {
         return 1;
     }
+
+    if !map_val.is_none() {
+        return map_val.unwrap().clone();
+    }
+
     let as_str = stone.to_string();
 
     if stone == 0 {
-        return process_stone(blink_times, blinks + 1, 1);
+        let retval = process_stone(blink_times, blinks + 1, 1, cache);
+        cache.insert((stone, blinks), retval);
+        return retval;
     } else if as_str.len() % 2 == 0 {
         let first_s = &as_str[..(as_str.len() / 2)];
         let second_s = &as_str[(as_str.len() / 2)..as_str.len()];
         let first = first_s.parse::<i64>().expect("First not a number!");
         let second = second_s.parse::<i64>().expect("Second not a number!");
-        return process_stone(blink_times, blinks + 1, first)
-            + process_stone(blink_times, blinks + 1, second);
+        let retval = process_stone(blink_times, blinks + 1, first, cache)
+            + process_stone(blink_times, blinks + 1, second, cache);
+        cache.insert((stone, blinks), retval);
+        return retval;
     } else {
-        return process_stone(blink_times, blinks + 1, stone * 2024);
+        let retval = process_stone(blink_times, blinks + 1, stone * 2024, cache);
+        cache.insert((stone, blinks), retval);
+        return retval;
     }
 }
 
 fn puzzle(stones: &Vec<i64>, blink_times: i32) -> i64 {
+    let mut cache: HashMap<(i64, i32), i64> = HashMap::new();
     let mut sum = 0;
+
     for stone in stones {
-        sum += process_stone(blink_times, 0, *stone);
+        sum += process_stone(blink_times, 0, *stone, &mut cache);
     }
 
     sum
-
-    // for i in 0..blink_times {
-    //     let mut s = 0;
-    //     while s < new_stones.len() {
-    //         let val = new_stones[s];
-    //         let as_str = val.to_string();
-    //         if val == 0 {
-    //             new_stones[s] = 1;
-    //             s += 1;
-    //         } else if as_str.len() % 2 == 0 {
-    //             let first = &as_str[..(as_str.len() / 2)];
-    //             let second = &as_str[(as_str.len() / 2)..as_str.len()];
-    //             new_stones[s] = first.parse::<i64>().expect("First not a number!");
-    //             new_stones.insert(s + 1, second.parse::<i64>().expect("Second not a number!"));
-    //             s += 2;
-    //         } else {
-    //             new_stones[s] *= 2024 as i64;
-    //             s += 1;
-    //         }
-    //     }
-    // }
 }
 
 fn main() {
